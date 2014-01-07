7d79298072e75909af9f7a3097173503bad0d0c9
sigmonky <sigmonky@gmail.com>
2014-01-07 09:10:52 -0500

    chapter 3 revisions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,22 +2,28 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	let(:base_title) { "Rails Tutorial Sample App" }
+
 
 	describe "Home page" do
 
 		it "should have the content 'Sample App'" do
-		  visit '/static_pages/home'
-		  expect(page).to have_content('Sample App')
+			visit '/static_pages/home'
+			expect(page).to have_content('Sample App')
 		end
 
-		it "should have the title 'Home'" do
-		  visit '/static_pages/home'
-		  expect(page).to have_title("#{base_title} | Home")
-		end
+		it "should have the base title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("#{base_title}")
 		end
 
-		describe "Help page" do
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title('| Home')
+		end
+	end
+
+	describe "Help page" do
 
 		it "should have the content 'Help'" do
 		  visit '/static_pages/help'
@@ -41,9 +47,9 @@ describe "Static pages" do
 		  visit '/static_pages/about'
 		  expect(page).to have_title("#{base_title} | About")
 		end
-		end
+	end
 
-		describe "Contact page" do
+	describe "Contact page" do
 
 		it "should have the content 'Contact'" do
 		  visit '/static_pages/contact'
