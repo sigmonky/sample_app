d6402eb371f2574c636f3eaf827ce3dc2dc88027
sigmonky <sigmonky@gmail.com>
2014-01-07 17:11:13 -0500

    minor changes to home page and application_helper.fullTitle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Rails Tutorial Sample App"
+    base_title = "Sample App"
     if page_title.empty?
       base_title
     else
